fb5349c91b837eb666ce41d194a68d7908da460d
韩忠涛 <hanzi@beagledata.com>
2025-10-26 14:22:02 +0800

    'init'

---
+++ src/main/java/com/gs/guoaiagent/GuoAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.gs.guoaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuoAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuoAiAgentApplication.class, args);
+    }
+
+}
